40b10087dcce04bbd039de30a7a67f88cf1d7883
uchel09 <ucelrussel77@gmail.com>
2025-01-02 22:25:48 +0800

    fix ukuran responsive huruf

--- src/view/About.jsx
+++ src/view/About.jsx
@@ -7,8 +7,8 @@ const About = () => {
           ABOUT ME
         </h1>
       </div>
-      <div className="group relative flex justify-center items-center bg-[#ffff45] text-black p-8 rounded-lg shadow-white shadow-2xl overflow-hidden">
-        <span className="[word-spacing:0.3rem] text-[20px] font-bold relative z-10 transition-colors duration-500 group-hover:text-white">
+      <div className="group relative flex justify-center items-center bg-[#ffff45] text-black p-6 rounded-lg shadow-white shadow-2xl overflow-hidden">
+        <span className="[word-spacing:0.15rem] sm:text-[14px] md:text-[18px] font-bold relative z-10 transition-colors duration-500 group-hover:text-white">
           A graduate of computer network engineering (TKJ) in 2024. Have an
           interest in the field of Website and Mobile Development. Have
           participated in MERN training, and learned MVC to help organize code,
@@ -22,22 +22,22 @@ const About = () => {
         <div className="absolute inset-0 bg-gradient-to-r from-[#ff014f] to-[#011eff] transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out"></div>
       </div>
       <div className="flex gap-5 overflow-x-auto w-full justify-evenly p-3">
-        <div className="group relative w-[200px] h-[80px] bg-[#ffff45] cursor-pointer rounded-xl shadow-white shadow-2xl overflow-hidden">
-          <span className="text-black font-bold md:text-[25px] text-[18px] text-center relative z-10 transition-colors duration-500 group-hover:text-white flex items-center justify-center h-full">
+        <div className="group relative w-[200px] h-[70px] bg-[#ffff45] cursor-pointer rounded-xl shadow-white shadow-2xl overflow-hidden">
+          <span className="text-black font-bold md:text-[25px] text-[15px] text-center relative z-10 transition-colors duration-500 group-hover:text-white flex items-center justify-center h-full">
             Projects 6
           </span>
           <div className="absolute inset-0 bg-gradient-to-r from-[#ff014f] to-[#011eff] transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out"></div>
         </div>
-        <div className="group relative w-[200px] h-[80px] bg-[#ffff45] cursor-pointer rounded-xl shadow-white shadow-2xl overflow-hidden">
-          <span className="text-black font-bold md:text-[25px] text-[18px] text-center relative z-10 transition-colors duration-500 group-hover:text-white flex items-center justify-center h-full">
+        <div className="group relative w-[200px] h-[70px] bg-[#ffff45] cursor-pointer rounded-xl shadow-white shadow-2xl overflow-hidden">
+          <span className="text-black font-bold md:text-[25px] text-[15px] text-center relative z-10 transition-colors duration-500 group-hover:text-white flex items-center justify-center h-full">
             Experience 1+
           </span>
           
           <div className="absolute inset-0 bg-gradient-to-r from-[#ff014f] to-[#011eff] transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out"></div>
         </div>
 
-        <div className="group relative w-[200px] h-[80px] bg-[#ffff45] cursor-pointer rounded-xl shadow-white shadow-2xl overflow-hidden">
-          <span className="text-black font-bold md:text-[25px] text-[18px] text-center relative z-10 transition-colors duration-500 group-hover:text-white flex items-center justify-center h-full">
+        <div className="group relative w-[200px] h-[70px] bg-[#ffff45] cursor-pointer rounded-xl shadow-white shadow-2xl overflow-hidden">
+          <span className="text-black font-bold md:text-[25px] text-[15px] text-center relative z-10 transition-colors duration-500 group-hover:text-white flex items-center justify-center h-full">
             Clients
             1
           </span>
